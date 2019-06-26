51fcda174398155acfa776b481d683c60cc8246a
Marcos <kakoferrare87@gmail.com>
2019-06-25 22:44:27 -0300

    Personalizando o retorno dos erros

--- src/api/billingCycle/billingCycle.js
+++ src/api/billingCycle/billingCycle.js
@@ -2,21 +2,21 @@ const restfull = require('node-restful')
 const mongoose = restfull.mongoose
 
 const creditSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    value: { type: Number, min:0, required: true }
+    name: { type: String, required:[true, 'Informe o nome para identificar o crédito'] },
+    value: { type: Number, min:[0, 'O valor informado é meno que o limite mínimo'], required: [true, 'Informe o valor'] }
 }) 
 const debitSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    value: { type: Number, min:0, required: true },
+    name: { type: String, required: [true, 'Informe o nome para identificar o débito'] },
+    value: { type: Number, min:[0, 'O valor informado é meno que o limite mínimo'], required: [true, 'Informe o valor'] },
     status: { type: String, required: false, uppercase: true,
         enum:['PAGO','PENDENTE','AGENDADO'] 
     }
 }) 
 
 const billingCycleSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    month: { type: Number, min:1, max:12, required: true },
-    year: { type: Number, min: 1970, max: 2100, required: true },
+    name: { type: String, required: [true, 'Informe o nome'] },
+    month: { type: Number, min:[1, 'O valor informado é meno que o limite mínimo'], max:[12, 'O valor informado é meno que o limite máximo'], required: [true, 'Informe o mês'] },
+    year: { type: Number, min: [1970, 'O valor informado é meno que o limite mínimo'], max: [2100, 'O valor informado é meno que o limite maximo'], required: [true, 'Informe o ano'] },
     credits: [creditSchema],
     debits: [debitSchema]
 })
--- src/config/database.js
+++ src/config/database.js
@@ -2,3 +2,5 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 module.exports = mongoose.connect('mongodb://admin:senhaadmin@localhost:27017/admin/mymoney')
+
+//mongoose.Error.messages.general.required = "O atributo {PATH} é obrigatório."
