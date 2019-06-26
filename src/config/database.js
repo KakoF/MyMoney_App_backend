e0158931b12c3fc8b231d8f76498472c7758307c
Marcos <kakoferrare87@gmail.com>
2019-06-26 20:36:12 -0300

    configuração para banco local

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://admin:senhaadmin@localhost:27017/admin/mymoney')
+//module.exports = mongoose.connect('mongodb://admin:senhaadmin@localhost:27017/admin/mymoney')
+module.exports = mongoose.connect('mongodb://localhost/mymoney', {useMongoClient: true,})
 
 //mongoose.Error.messages.general.required = "O atributo {PATH} é obrigatório."
