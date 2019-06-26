4bffd218f05bcc3acc67e619f3b10f1693baca1e
KakoF <kakoferrare87@gmail.com>
2019-06-26 12:28:28 -0300

    api que retorna o contador

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -3,4 +3,14 @@ const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get','post','put','delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((erro, value) => {
+        if(erro){
+            res.status(500).json({errors: [erro]})
+        }else{
+            res.json({value})
+        }
+    })
+})
+
 module.exports = BillingCycle
