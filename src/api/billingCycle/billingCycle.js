06c07d0239202cfb602e14d9de39064e26b9768b
Marcos <kakoferrare87@gmail.com>
2019-06-26 20:02:37 -0300

    Atualização de dependencia do mongo, tratativas nos erros e novas API's

--- src/api/billingCycle/billingCycle.js
+++ src/api/billingCycle/billingCycle.js
@@ -3,11 +3,11 @@ const mongoose = restfull.mongoose
 
 const creditSchema = new mongoose.Schema({
     name: { type: String, required:[true, 'Informe o nome para identificar o crédito'] },
-    value: { type: Number, min:[0, 'O valor informado é meno que o limite mínimo'], required: [true, 'Informe o valor'] }
+    value: { type: Number, min:[0, 'O valor informado é menor que o limite mínimo'], required: [true, 'Informe o valor'] }
 }) 
 const debitSchema = new mongoose.Schema({
     name: { type: String, required: [true, 'Informe o nome para identificar o débito'] },
-    value: { type: Number, min:[0, 'O valor informado é meno que o limite mínimo'], required: [true, 'Informe o valor'] },
+    value: { type: Number, min:[0, 'O valor informado é menor que o limite mínimo'], required: [true, 'Informe o valor'] },
     status: { type: String, required: false, uppercase: true,
         enum:['PAGO','PENDENTE','AGENDADO'] 
     }
@@ -15,8 +15,8 @@ const debitSchema = new mongoose.Schema({
 
 const billingCycleSchema = new mongoose.Schema({
     name: { type: String, required: [true, 'Informe o nome'] },
-    month: { type: Number, min:[1, 'O valor informado é meno que o limite mínimo'], max:[12, 'O valor informado é meno que o limite máximo'], required: [true, 'Informe o mês'] },
-    year: { type: Number, min: [1970, 'O valor informado é meno que o limite mínimo'], max: [2100, 'O valor informado é meno que o limite maximo'], required: [true, 'Informe o ano'] },
+    month: { type: Number, min:[1, 'O valor informado é menor que o limite mínimo'], max:[12, 'O valor informado é maior que o limite máximo'], required: [true, 'Informe o mês'] },
+    year: { type: Number, min: [1970, 'O valor informado é menor que o limite mínimo'], max: [2100, 'O valor informado é maior que o limite maximo'], required: [true, 'Informe o ano'] },
     credits: [creditSchema],
     debits: [debitSchema]
 })
--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -1,14 +1,33 @@
 const BillingCycle = require('./billingCycle')
+const errorHandler = require('../common/errorHandler')
 
 BillingCycle.methods(['get','post','put','delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
+BillingCycle.after('post', errorHandler).after('put', errorHandler)
+
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((erro, value) => {
         if(erro){
-            res.status(500).json({errors: [erro]})
+            res.status(500).json({errors: [error]})
         }else{
             res.json({value})
+        }
+    })
+})
+
+BillingCycle.route('summary', (req, res, next)=>{
+    BillingCycle.aggregate({
+        $project: { credit: {$sum: "$credits.value"}, debit: {$sum: "$debits.value"}}
+    },{
+        $group: {_id: null, credit: {$sum: "$credit"}, debit: {$sum: "debit"}}
+    },{
+        $project: {_id: 0, credit: 1, debit: 1}
+    }, (error, result) =>{
+        if(error){
+            res.status(500).json({errors: [error]})
+        }else{
+            res.json(result[0] || {credit: 0, debit: 0})
         }
     })
 })
